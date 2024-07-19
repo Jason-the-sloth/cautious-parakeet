c3a598eb3426658268e72e793d3a6275dec32f37
Tiyani Mabunda <Tiyani.Mabunda@lifehealthcare.co.za>
2024-07-19 12:29:19 +0200

    Scoring implementation

--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -17,6 +17,7 @@ public class GlobalVariables : ScriptableObject
     public  float viewAngle;
     public float maxHealth = 10f;
     public GameObject bullet;
+    public GameObject stats;
 
     [Space(10)]
     [Header("Player Set Up")]
